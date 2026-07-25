add80c0e1d07160a986987b0eb5d0bdc476c3e79
Ashraf Al Arbid <127553105+Reach-ArbidozaTech@users.noreply.github.com>
2026-07-25 21:18:58 +0200

    Initial commit

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,9 @@
+
+public class Sandbox{
+    public static void main(String [] args){//start point our programm
+        // this is a comment
+        System.out.println("Ada Lovelace : D");
+        System.out.println("Hello World!");
+        System.out.println("Hey There !");
+    }// end of main
+}
